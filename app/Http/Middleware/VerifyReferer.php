afbfd8ba0d4bbaac716f9fe0fa60f2dc56c0ba67
Aron-tech <aron.papp2003@gmail.com>
2025-03-03 09:07:38 +0100

    Add middlewar for widget API

---
+++ app/Http/Middleware/VerifyReferer.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use App\Models\Site;
+use Illuminate\Support\Facades\Log;
+
+class VerifyReferer
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        $referer = $request->server('HTTP_REFERER');
+
+        //Log::info($referer);
+
+        $uuid = $request->route('site')['uuid'];
+
+        $site = Site::where('uuid', $uuid)->firstOrFail();
+
+        if ($referer !== $site->domain) {
+            return abort(403, 'Érvénytelen azonosító!');
+            //Log::error('Hozzários megtagadva, mert a referer nem egyezik a webhely URL-jelvel: ' . $referer . ' != ' . $site->domain);
+        }
+
+        return $next($request);
+    }
+}
