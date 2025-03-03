167fae7897378d5e6bde3b044f99a8b94f13f883
Aron-tech <aron.papp2003@gmail.com>
2025-03-03 13:37:11 +0100

    Add back to page function for filament admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -12,6 +12,7 @@ use Filament\Panel;
 use Filament\PanelProvider;
 use Filament\Support\Colors\Color;
 use Filament\Widgets;
+use Filament\Navigation\MenuItem;
 use Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse;
 use Illuminate\Cookie\Middleware\EncryptCookies;
 use Illuminate\Foundation\Http\Middleware\VerifyCsrfToken;
@@ -27,6 +28,12 @@ class AdminPanelProvider extends PanelProvider
             ->default()
             ->id('admin')
             ->path('admin')
+            ->userMenuItems([
+                MenuItem::make('Home')
+                    ->label('Vissza a weboldalra')
+                    ->url('/') // A főoldal URL-je
+                    ->icon('heroicon-o-home'),
+            ])
             ->login()
             ->colors([
                 'primary' => Color::Amber,
