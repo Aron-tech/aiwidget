955fd203788a39f07602cf908fe0dc95b8f93c45
Aron-tech <aron.papp2003@gmail.com>
2025-03-05 16:50:07 +0100

    Fixed API middleware

--- routes/api.php
+++ routes/api.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\ChatController;
 use App\Http\Controllers\MessageController;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware(['throttle:1000,1', 'verify.referer'])->name('widget.')->group(function () {
+Route::middleware(['throttle:1000,1', \App\Http\Middleware\VerifyReferer::class])->name('widget.')->group(function () {
 
     Route::post('/submit-message/{site:uuid}', [MessageController::class, 'storeUserMessage'])->name('store');
     Route::get('/messages/{site:uuid}', [ChatController::class, 'show'])->name('show');
