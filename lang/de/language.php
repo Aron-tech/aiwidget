e621c9ce7298abbffec3f86d36720e4100b656ba
Aron-tech <aron.papp2003@gmail.com>
2025-03-24 18:00:26 +0100

    Add language files to language page

---
+++ lang/de/language.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'languages' => 'Sprachen',
+    'select' => 'Wählen Sie Ihre Sprache',
+    'en' => 'Englisch',
+    'de' => 'Deutsch',
+    'hu' => 'Ungarisch',
+    'updated' => 'Erfolgreich aktualisiert!',
+];
---
+++ lang/en/language.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'languages' => 'Languages',
+    'select' => 'Select your language',
+    'en' => 'English',
+    'de' => 'Deutsch',
+    'hu' => 'Hungarian',
+    'updated' => 'Successfully updated!',
+];
