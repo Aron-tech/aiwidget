5425a5e947fec0249cc2dee1be8171998d1cffdb
Aron-tech <aron.papp2003@gmail.com>
2025-03-24 19:37:54 +0100

    Add default widget style and panel components

---
+++ resources/views/components/widget-style/panel.blade.php
@@ -0,0 +1,15 @@
+<div class="splide widget-styles flex flex-col items-center justify-center" aria-labelledby="carousel-heading" wire:ignore>
+    <div class="splide__track w-full max-w-md">
+        <ul class="splide__list">
+            <li class="splide__slide flex justify-center">
+                <x-widget-style.default/>
+            </li>
+            <li class="splide__slide flex justify-center">Slide 02</li>
+            <li class="splide__slide flex justify-center">Slide 03</li>
+        </ul>
+    </div>
+    <div class="flex justify-center gap-4 mt-4">
+        <flux:button class="widget-style-prev" icon="arrow-long-left"></flux:button>
+        <flux:button class="widget-style-next" icon="arrow-long-right"></flux:button>
+    </div>
+</div>
