8cedeb9ba2332e30f3b6004fb0a2b2744cc8cf09
Aron-tech <aron.papp2003@gmail.com>
2025-04-16 10:23:29 +0200

    Add trait

---
+++ app/Models/Traits/Searchable.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models\Traits;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait Searchable
+{
+    public function scopeSearch(Builder $query, string|array $search): void
+    {
+        $search = explode(' ', (string)$search);
+
+        $query->where(function (Builder $query) use ($search) {
+            foreach ($search as $word) {
+                foreach ($this->searchable as $column) {
+                    $query->orWhere($column, 'LIKE', '%' . $word . '%');
+                }
+            }
+        });
+    }
+}
