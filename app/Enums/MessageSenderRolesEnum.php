ad35107e529d1f2180dcee9f2772174942503cd5
Aron-tech <aron.papp2003@gmail.com>
2025-05-27 10:23:28 +0200

    Add getLabel function

--- app/Enums/MessageSenderRolesEnum.php
+++ app/Enums/MessageSenderRolesEnum.php
@@ -1,10 +1,18 @@
 <?php
 
 namespace App\Enums;
-
 enum MessageSenderRolesEnum: string
 {
     case BOT = 'bot';
     case USER = 'user';
     case ADMIN = 'admin';
+
+    public function getLabel(): string
+    {
+        return match ($this) {
+            self::BOT => __('Bot'),
+            self::USER => __('User'),
+            self::ADMIN => __('Admin'),
+        };
+    }
 }
